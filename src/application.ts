310c7fce34bae4955f80bdb1023bfe430eefb348
SFIN587 <as19ish@gmail.com>
2020-12-12 13:06:35 +0530

    addded dotenv, allowed origin, logged request and response

--- src/application.ts
+++ src/application.ts
@@ -1,12 +1,14 @@
 import {BootMixin} from '@loopback/boot';
 import {ApplicationConfig} from '@loopback/core';
-import {
-  RestExplorerBindings,
-  RestExplorerComponent,
-} from '@loopback/rest-explorer';
 import {RepositoryMixin} from '@loopback/repository';
 import {RestApplication} from '@loopback/rest';
+import {
+  RestExplorerBindings,
+  RestExplorerComponent
+} from '@loopback/rest-explorer';
 import {ServiceMixin} from '@loopback/service-proxy';
+import * as dotenv from 'dotenv';
+import * as dotenvExt from 'dotenv-extended';
 import path from 'path';
 import {MySequence} from './sequence';
 
@@ -16,6 +18,21 @@ export class UserCrudApplication extends BootMixin(
   ServiceMixin(RepositoryMixin(RestApplication)),
 ) {
   constructor(options: ApplicationConfig = {}) {
+    dotenv.config();
+    dotenvExt.load({
+      schema: '.env.default',
+      errorOnMissing: true,
+      includeProcessEnv: true,
+    });
+    options.rest = options.rest || {};
+    options.rest.cors = {
+      origin: process.env.ALLOWED_ORIGIN,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      maxAge: 86400,
+      credentials: true,
+    };
     super(options);
 
     // Set up the custom sequence
@@ -35,7 +52,7 @@ export class UserCrudApplication extends BootMixin(
     this.bootOptions = {
       controllers: {
         // Customize ControllerBooter Conventions here
-        dirs: ['controllers'],
+        dirs: ['controllers', 'modules'],
         extensions: ['.controller.js'],
         nested: true,
       },
--- src/sequence.ts
+++ src/sequence.ts
@@ -1,3 +1,34 @@
-import {MiddlewareSequence} from '@loopback/rest';
+import {DefaultSequence, RequestContext} from '@loopback/rest';
 
-export class MySequence extends MiddlewareSequence {}
+export class MySequence extends DefaultSequence {
+  log(msg: string) {
+    console.log(msg);
+  }
+  async handle(context: RequestContext) {
+    const requestTime = Date.now();
+    const {request} = context;
+    try {
+      this.log(
+        `Request ${request.method} ${request.url
+        } started at ${requestTime.toString()}.
+          Request Details
+          Referer = ${request.headers.referer}
+          User-Agent = ${request.headers['user-agent']}
+          Remote Address = ${request.connection.remoteAddress}
+          Remote Address (Proxy) = ${request.headers['x-forwarded-for']}`,
+      );
+      await super.handle(context);
+
+    } catch (err) {
+      this.log(
+        `Request ${request.method} ${request.url
+        } errored out. Error :: ${JSON.stringify(err)} ${err}`,
+      );
+    } finally {
+      this.log(
+        `Request ${request.method} ${request.url
+        } Completed in ${Date.now() - requestTime}ms`,
+      );
+    }
+  }
+}
